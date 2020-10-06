4d92627e1003a786458adf71d7cf842bedbbc5b8
michaelawyu <chenyumic@google.com>
2020-10-06 12:13:49 -0700

    Updated the generator to use the revised qt tool and the new schema org

--- gen/src/index.ts
+++ gen/src/index.ts
@@ -11,10 +11,6 @@ let OUT = yargs.argv.out || process.env.OUT;
 
 const LANGUAGE = 'go';
 const TEMPLATE_DIRECTORY = 'templates';
-const GO_FILENAME = 'events.go';
-const SNIPPET_DIRECTORY = 'snippets';
-const MARSHAL_FUNCS_TO_REPLACE_SNIPPET = 'marshalFuncsToReplace.snippet';
-const MARSHAL_FUNCS_TEMPLATE = 'marshalFuncs.squirrelly';
 const README_TEMPLATE = 'README.squirrelly';
 
 interface Event {
@@ -31,7 +27,6 @@ async function main() {
   if (OUT.endsWith('/')) OUT = OUT.substring(0, OUT.length - 1);
 
   const templateDirectoryPath = `${__dirname}/../../${TEMPLATE_DIRECTORY}`;
-  const snippetDirectoryPath = `${__dirname}/../../${SNIPPET_DIRECTORY}`;
 
   // Collect all schemas and generate Go code from them using the
   // quicktype-wrapper package
@@ -43,6 +38,7 @@ async function main() {
     pkg = pkg.replace('google.events.', '');
     const pkgPath = pkg.replace(/\./g, '/');
     mkdirp.sync(`${OUT}/${pkgPath}`);
+    const eventName = schema.name;
 
     // Update the package statement
     const goPkg = schema['goPackage'];
@@ -50,31 +46,15 @@ async function main() {
     genFile = genFile.replace('package main', goPkgStmt);
 
     // Collect all events in the schema
-    const pkgEvents: string[] = [];
-    Object.keys(schema.properties).map((eventName: string) => {
-      const eventDescription = schema.properties[eventName].description;
-      pkgEvents.push(eventName);
-      allEvents.push({
-        package: goPkg,
-        eventName: eventName,
-        eventDescription: eventDescription,
-      });
+    const eventDescription = schema.description;
+    allEvents.push({
+      package: goPkg,
+      eventName: eventName,
+      eventDescription: eventDescription,
     });
-
-    // Replace the utility funcs in the generated
-    const marshalFuncsSnippet = fs.readFileSync(
-      `${snippetDirectoryPath}/${MARSHAL_FUNCS_TO_REPLACE_SNIPPET}`
-    );
-    const marshalFuncsTmpl = fs.readFileSync(
-      `${templateDirectoryPath}/${MARSHAL_FUNCS_TEMPLATE}`
-    );
-    const marshalFuncs = sqrl.render(String(marshalFuncsTmpl), {
-      pkgEvents: pkgEvents,
-    });
-    genFile = genFile.replace(String(marshalFuncsSnippet), marshalFuncs);
 
     // Write the patched generated code
-    fs.writeFileSync(`${OUT}/${pkgPath}/${GO_FILENAME}`, genFile);
+    fs.writeFileSync(`${OUT}/${pkgPath}/${eventName}.go`, genFile);
   });
 
   // Write the README.md file
