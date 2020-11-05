78f7229dad2eaf6fdcba00ccabbe329647e74290
Grant Timmerman <744973+grant@users.noreply.github.com>
2020-11-05 12:26:13 -0600

    feat: simplify gen steps, run the generator (#16)
    * feat: simplify gen instructions
    
    Signed-off-by: Grant Timmerman <timmerman+devrel@google.com>
    
    * docs: remove type index
    
    Signed-off-by: Grant Timmerman <timmerman+devrel@google.com>
    
    * feat: update generator; generate
    
    Signed-off-by: Grant Timmerman <timmerman+devrel@google.com>
    
    * fix: ignore build dir
    
    Signed-off-by: Grant Timmerman <timmerman+devrel@google.com>
    
    * fix: package json dep v dev deps
    
    Signed-off-by: Grant Timmerman <timmerman+devrel@google.com>

---
+++ cloud/pubsub/v1/message_published_data.go
@@ -0,0 +1,39 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package pubsub
+
+import "encoding/json"
+
+// The data received in an event when a message is published to a topic.
+type MessagePublishedData struct {
+	Message      *Message `json:"message,omitempty"`     // The message that was published.
+	Subscription *string  `json:"subscription,omitempty"`// The resource name of the subscription for which this event was; generated. The format of the value is; `projects/{project-id}/subscriptions/{subscription-id}`.
+}
+
+// The message that was published.
+type Message struct {
+	Attributes map[string]string `json:"attributes,omitempty"`// Attributes for this message.
+	Data       *string           `json:"data,omitempty"`      // The binary data in the message.
+	MessageID  *string           `json:"messageId,omitempty"` // ID of this message, assigned by the server when the message is published.; Guaranteed to be unique within the topic.
+}
+
+func UnmarshalMessagePublishedData(data []byte) (MessagePublishedData, error) {
+	var d MessagePublishedData
+	err := json.Unmarshal(data, &d)
+	return d, err
+}
+
+func (p *MessagePublishedData) MarshalMessagePublishedData() ([]byte, error) {
+	return json.Marshal(p)
+}
---
+++ firebase/database/v1/reference_event_data.go
@@ -0,0 +1,32 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package database
+
+import "encoding/json"
+
+// The data within all Firebase Real Time Database reference events.
+type ReferenceEventData struct {
+	Data  map[string]interface{} `json:"data"` 
+	Delta map[string]interface{} `json:"delta"`
+}
+
+func UnmarshalReferenceEventData(data []byte) (ReferenceEventData, error) {
+	var d ReferenceEventData
+	err := json.Unmarshal(data, &d)
+	return d, err
+}
+
+func (p *ReferenceEventData) MarshalReferenceEventData() ([]byte, error) {
+	return json.Marshal(p)
+}
