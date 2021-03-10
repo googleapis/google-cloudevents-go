f7b397df540a5ae37a978178aeac2eb0f123aa68
Grant Timmerman <timmerman+devrel@google.com>
2021-03-10 11:42:27 -0600

    feat: runs the generator, which fixes the json name for some fields with numbers
    Signed-off-by: Grant Timmerman <timmerman+devrel@google.com>

--- firebase/auth/v1/auth_event_data.go
+++ firebase/auth/v1/auth_event_data.go
@@ -24,7 +24,7 @@ type AuthEventData struct {
 	EmailVerified *bool           `json:"emailVerified,omitempty"` // Whether or not the user's primary email is verified.
 	Metadata      *Metadata       `json:"metadata,omitempty"`      // Additional metadata about the user.
 	PhoneNumber   *string         `json:"phoneNumber,omitempty"`   // The user's phone number.
-	PhotoURL      *string         `json:"photoUrl,omitempty"`      // The user's photo URL.
+	PhotoURL      *string         `json:"photoURL,omitempty"`      // The user's photo URL.
 	ProviderData  []ProviderDatum `json:"providerData,omitempty"`  // User's info at the providers
 	Uid           *string         `json:"uid,omitempty"`           // The user identifier in the Firebase app.
 }
@@ -45,7 +45,7 @@ type Metadata struct {
 type ProviderDatum struct {
 	DisplayName *string `json:"displayName,omitempty"` // The display name for the linked provider.
 	Email       *string `json:"email,omitempty"`       // The email for the linked provider.
-	PhotoURL    *string `json:"photoUrl,omitempty"`    // The photo URL for the linked provider.
+	PhotoURL    *string `json:"photoURL,omitempty"`    // The photo URL for the linked provider.
 	ProviderID  *string `json:"providerId,omitempty"`  // The linked provider ID (e.g. "google.com" for the Google provider).
 	Uid         *string `json:"uid,omitempty"`         // The user identifier for the linked provider.
 }
